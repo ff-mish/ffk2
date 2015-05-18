e01d636038caf645d41ca24e3a67e9cf2f108d29
fenly.su <fenly.su@fredfarid.com>
2015-05-18 19:02:05 +0800

    ffk2

--- js/main.js
+++ js/main.js
@@ -99,6 +99,8 @@ $(function () {
         mapCircle();
 
         $('.indexMap').css('height', $(window).height() + 'px');
+
+        $('.low-browser').css('height', $(window).height() + 'px');
     });
 
     $('#slideAlink').click(function () {
@@ -335,8 +337,54 @@ $(function () {
 function myBrowser() {
     var userAgent = navigator.userAgent; //取得浏览器的userAgent字符串
     var isSafari = userAgent.indexOf("Safari") > -1 && userAgent.indexOf("Chrome") < 1; //判断是否Safari
+    var isIE = userAgent.indexOf("compatible") > -1 && userAgent.indexOf("MSIE") > -1 && !isOpera ; //判断是否IE
     if (isSafari) {
         $('.contact_links a').css('font-weight', '300');
+    }
+    if(isIE){
+        var IE5 = IE55 = IE6 = IE7 = IE8 = false;
+        var reIE = new RegExp("MSIE (+);");
+        reIE.test(userAgent);
+        var fIEVersion = parseFloat(RegExp["$1"]);
+
+        IE55 = fIEVersion == 5.5 ;
+        IE6 = fIEVersion == 6.0 ;
+        IE7 = fIEVersion == 7.0 ;
+        IE8 = fIEVersion == 8.0 ;
+
+        if(IE55){
+            $('.s_all').css({
+                'display':'none'
+            });
+            $('.low-browser').css({
+                'display':'block'
+            });
+        }
+        if(IE6){
+            $('.s_all').css({
+                'display':'none'
+            });
+            $('.low-browser').css({
+                'display':'block'
+            });
+        }
+        if(IE7){
+            $('.s_all').css({
+                'display':'none'
+            });
+            $('.low-browser').css({
+                'display':'block'
+            });
+        }
+        if(IE8){
+            $('.s_all').css({
+                'display':'none'
+            });
+            $('.low-browser').css({
+                'display':'block'
+            });
+        }
+
     }
 }
 //myBrowser() end
